279bdf2b9650283455a392cc272f581381e27828
Demitrus Booker <demitrusw.b@gmail.com>
2023-06-22 05:37:34 -0700

    turned the header fields into a dictionary

--- bootcamp_python/quests/quest06/ex02/my_data_process.py
+++ bootcamp_python/quests/quest06/ex02/my_data_process.py
@@ -25,43 +25,50 @@ def delete_fields(arr):
     
     return new_list
 
-def my_data_process(arr):
-    # print(arr)
-    
-    # remove elements that are not needed
-    deleted = delete_fields(arr)
-    
-    # first item to be zipped 
-    header_line = deleted[0].split(',')
-    deleted.pop(0)
+# def place_into_dict(dict, element):
 
-  
-    # second item to be zipped
+def my_data_process(arr):
+    
+    #remove field and assoc data for the field that are not needed
+    deleted = delete_fields(arr)   
+
+    # print(deleted)
+    #remove header 
+    header_line_removal = deleted[0].split(',')
+    
+    # print(header_line_removal)
+
+    #input header fields into a dict
+    header_dict = {key: None for key in header_line_removal}
+    # print(header_dict)
+    header = deleted.pop(0)
+    # print(deleted)
+    # print(header_dict)
+
+    # ---------------------------
+        #turn multiple values belonging to one key into a list ---> use switch statement
+            #aka [male:3, female:2]
+        #assign the key to each newly created list of values
+    # ---------------------------
+
     a = {}
     i = 0
     while i < len(deleted):
-    # for item in deleted:
+        #taking each element and splitting into individual strings
         line = deleted[i].split(',')
         j = 0
+        #input each element into the dicitionary
         for word in line:
             if word in a:
                 a[word] += 1
             else:
                 a[word] = 1
-        break
+        # break
         i += 1
     
-    
-    D = dict(zip(header_line, a))
-    print(D)
-
-
-
-
-
-
-
-
+    # D = dict(zip(header_line_removal, a))
+    print("\n")
+    print(a)
 
 my_data_transform = ['Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At', 'Male,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon', 'Male,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon', 'Female,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon', 'Female,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning', 'Male,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon']
 result = my_data_process(my_data_transform)
